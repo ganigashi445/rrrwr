0cf50a5e1774c2ca7341220a76b98b7053fbe0da
ganigashi445 <146036267+ganigashi445@users.noreply.github.com>
2023-10-03 02:10:49 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './components/Home';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from './components/About';
+import Rwr from './components/Rwr';
+
+
+
+const root1 = ReactDOM.createRoot(document.getElementById('root'));
+const root2 = ReactDOM.createRoot(document.getElementById('root1'));
+root1.render(
+  <React.StrictMode>
+  <Router>
+  <Routes> 
+      <Route path="/welcome" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/" element={<App />} />
+    </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+if (root2) {
+  root2.render(
+    <React.StrictMode>
+    <Router>
+    <Routes> 
+        <Route path="/rwr" element={<Rwr />} />
+      </Routes>
+      </Router>
+    </React.StrictMode>
+  );
+}
